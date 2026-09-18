31d6bdea5751037e23b4772e83434eefa226fb05
sasmitha-sudusinghe <sasmithasudusinghe@gmail.com>
2026-09-18 23:06:57 +0530

    updated

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -3,8 +3,8 @@ import profilepic from "./assets/image1.png"
 function Card(){
     return(
     <div className="card">
-        <img src={profilepic} alt="Profile Picture"></img>
-        <h2>Sasmitha Sudusinghe</h2>
+        <img className="card-image" src={profilepic} alt="Profile Picture"></img>
+        <h2 className="card-title">Sasmitha Sudusinghe</h2>
         <p>UG in University of Vavuniya</p>
     </div>
     );
