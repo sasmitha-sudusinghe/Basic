0b70c78d0962ed209c0bed49b78dd378385b5055
sasmitha-sudusinghe <sasmithasudusinghe@gmail.com>
2026-09-19 20:47:07 +0530

    Student updated

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,14 @@
 import Card from "./Card.jsx"
 import Button from "./Button.jsx"
+import Student from "./Student.jsx"
+
+
 function App() {
   return(
     <>
       <Card/>
       <Button/>
+      <Student name="Spongebob" age="23" isStudent={true}/>
     </>
   );
 }
